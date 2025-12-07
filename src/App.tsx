08dbb623923b12a363822da87e563041f93de1ec
Eoin Logue <eoinlogue@Eoins-MacBook-Air.local>
2025-12-07 21:15:06 +0000

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main style={{ minHeight: "100vh", display: "grid", placeItems: "center" }}>
+      <h1>Eoin&apos;s Presents – Coming Soon</h1>
+    </main>
+  );
+}
+
+export default App;
